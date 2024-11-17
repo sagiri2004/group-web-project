c9b249f5f8ef65eabda0baa71e3f28269fcbc309
quyetdaika <quyet.nguyenvan@kool-soft.com>
2024-11-17 22:52:22 +0700

    user-assignmet model & migration

---
+++ classroom-project-backend/src/models/userAssignment.js
@@ -0,0 +1,53 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class UserAssignment extends Model {
+    static associate(models) {
+      // Define associations here
+      UserAssignment.belongsTo(models.User, {
+        foreignKey: "userId",
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      });
+      UserAssignment.belongsTo(models.Assignment, {
+        foreignKey: "assignmentId",
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      });
+    }
+  }
+
+  UserAssignment.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      assignmentId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Assignments",
+          key: "id",
+        },
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "UserAssignment",
+      tableName: "Users_Assignments",
+      timestamps: true,
+    }
+  );
+
+  return UserAssignment;
+};
