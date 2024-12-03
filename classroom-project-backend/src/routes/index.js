c9ee8f981faddc8dda26c7dd45a2ba70164774b4
quyetdaika <quyet.nguyenvan@kool-soft.com>
2024-12-03 17:30:38 +0700

    flashcard route

--- classroom-project-backend/src/routes/index.js
+++ classroom-project-backend/src/routes/index.js
@@ -1,7 +1,12 @@
 const authRouter = require("./auth");
 const messageRouter = require("./message");
+const flashcardRouter = require("./flashcards");
 
 module.exports = (app) => {
   app.use("/api/auth", authRouter);
   app.use("/api/messages", messageRouter);
+  app.use("/api/flashcard", flashcardRouter);
+  app.use((req, res) => {
+    res.status(404).json({ error: "Route not found" });
+  });
 };
