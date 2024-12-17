6b6d2615d044dee5552b3e3dad377de39043a116
Sagiri <thang.nh225763@sis.hust.edu.vn>
2024-12-18 01:05:51 +0700

    debug + lam not nhung func con thieu

--- classroom-project-backend/src/index.js
+++ classroom-project-backend/src/index.js
@@ -9,8 +9,8 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// const allowedOrigins = ["https://group-web-project-omega.vercel.app"];
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["https://group-web-project-omega.vercel.app"];
+// const allowedOrigins = ["http://localhost:5173"];
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -58,8 +58,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    // origin: "https://group-web-project-omega.vercel.app",
-    origin: "http://localhost:5173",
+    origin: "https://group-web-project-omega.vercel.app",
+    // origin: "http://localhost:5173",
     methods: ["GET", "POST"],
     credentials: true,
   },
